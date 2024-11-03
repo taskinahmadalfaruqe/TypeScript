8c9f9aa7fb82cff32119d53d25c1fc1e435e7b55
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2024-11-03 18:05:50 +0800

    Add Tuple And Some Data Type.

--- Module-01/src/DataType.ts
+++ Module-01/src/DataType.ts
@@ -5,3 +5,32 @@ let StringData: string = "Name";
 
 //Number
 let NumberData: number = 10;
+
+// Bool
+let y: boolean = true;
+
+// undefined
+let undefinedData: undefined = undefined;
+
+// null
+let nullData: null = null;
+
+// Array
+// string type array
+let array01: string[] = ["Taskin", "Rana", "Sabbir"];
+
+// number type array
+let numberArray: number[] = [10, 20, 30, 40, 50];
+// insert a value in array
+numberArray.push(200);
+// numberArray.push("Number"); not possible to push any thing without number.
+
+// tupple
+let tupleArray: [number, string] = [20, "mr-x"];
+tupleArray[0] = 100;
+tupleArray[1] = "Taskin";
+console.log(tupleArray);
+
+let tupleArray2: [number, string] = [20, "mr-x"];
+tupleArray2 = [10, "Taskin"];
+console.log(tupleArray2);

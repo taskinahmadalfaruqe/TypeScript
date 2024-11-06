67f0684a6c28b4ca2d433d66c8613857834602db
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2024-11-06 23:50:11 +0800

    Add Type Assertion

--- Module-02/dist/TypeAssertion.js
+++ Module-02/dist/TypeAssertion.js
@@ -1,3 +1,11 @@
 "use strict";
-const data = "hello";
-console.log(data);
+{
+    // Block Scoping
+    // Type Assertion: Ami Developer Hisibe typeScript er theke besi type bujte para ke type assertion bole.
+    // Type Assertion
+    let anything;
+    anything = "Next level Development";
+    anything.split(" ");
+    console.log(anything);
+    // Block Scoping
+}
--- Module-02/src/TypeAssertion.ts
+++ Module-02/src/TypeAssertion.ts
@@ -1,2 +1,11 @@
-const data: string = "hello";
-console.log(data);
+{
+  // Block Scoping
+  // Type Assertion: Ami Developer Hisibe typeScript er theke besi type bujte para ke type assertion bole.
+
+  // Type Assertion
+  let anything: any;
+  anything = "Next level Development";
+  (anything as string).split(" ");
+  console.log(anything);
+  // Block Scoping
+}

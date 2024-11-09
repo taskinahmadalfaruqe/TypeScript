94d3a2f35ec71ca88594c0201a7b6834e323004f
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-11-09 20:31:58 +0600

    Update GenericWithInterface.ts

--- Module-02/src/GenericWithInterface.ts
+++ Module-02/src/GenericWithInterface.ts
@@ -21,6 +21,8 @@
   interface ProMobile {
     Name: string;
     Modle: string;
+Ram:string;
+Room:string;
   }
   const proDeveloper: developer<ProMobile> = {
     Computer: "Apple MackBook",
@@ -28,6 +30,8 @@
     mobile: {
       Name: "Iphone",
       Modle: "15 Pro Max",
+Ram: "8GB",
+Room:"256 GB"
     },
   };
   // Block Scoped

05e171a8005a0ab47670e34b853258b779d81949
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2024-11-17 18:59:21 +0800

    Add New Data

--- Module-02/src/mapType.ts
+++ Module-02/src/mapType.ts
@@ -1,5 +1,8 @@
 {
   // Blocked Scope
-  
+  const arrNumber: number[] = [1, 2, 3, 4, 3];
+  const arrString: string[] = ["1", "2", "3", "4"];
+  const arrString02: string[] = arrNumber.map((number) => number.toString());
+  console.log(arrString02);
   // Blocked Scope
 }

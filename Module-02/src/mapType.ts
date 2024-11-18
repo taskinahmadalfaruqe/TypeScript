d33e9f5ef056a95098e7f9e7d400ec96eb4550d1
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2024-11-18 21:03:59 +0800

    Add Mape type.

--- Module-02/src/mapType.ts
+++ Module-02/src/mapType.ts
@@ -4,5 +4,18 @@
   const arrString: string[] = ["1", "2", "3", "4"];
   const arrString02: string[] = arrNumber.map((number) => number.toString());
   console.log(arrString02);
+  type Area = {
+    height: number;
+    width: number;
+  };
+  // type AreaString = {
+  //   height: string;
+  //   width: string;
+  // };
+
+  type AreaString = {
+    [key in "height" | "width"]: string;
+  };
+
   // Blocked Scope
 }

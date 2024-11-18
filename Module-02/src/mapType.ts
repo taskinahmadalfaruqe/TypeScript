04487b087d6c6cd08ae858ac4dc2c9986312116f
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2024-11-18 21:13:12 +0800

    Add Full Map Function>

--- Module-02/src/mapType.ts
+++ Module-02/src/mapType.ts
@@ -8,13 +8,30 @@
     height: number;
     width: number;
   };
+  // look up
+  type Height = Area["height"];
+
   // type AreaString = {
   //   height: string;
   //   width: string;
   // };
 
-  type AreaString = {
-    [key in "height" | "width"]: string;
+  // type AreaString = {
+  //   [key in keyof "height" | "width"]: string;
+  // };
+
+  type AreaString01 = {
+    [key in keyof Area]: string;
+  };
+
+  // declear type with generic
+  type AreaString<T> = {
+    [key in keyof T]: T[key];
+  };
+
+  const AreaCalculate: AreaString<{ height: number; width: number }> = {
+    height: 100,
+    width: 20,
   };
 
   // Blocked Scope

6ad29aac3546f1d2a18344f89f395e4a0aff4cdb
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2024-11-21 21:23:30 +0800

    Add Class And Obj.

--- Module-03/src/ClassAndObj.ts
+++ Module-03/src/ClassAndObj.ts
@@ -4,6 +4,22 @@
     name: string;
     roll: number;
     StudyClass: string;
+    constructor(name: string, roll: number, StudyClass: string) {
+      this.name = name;
+      this.roll = roll;
+      this.StudyClass = StudyClass;
+    }
+    findStudent() {
+      console.log(
+        `Student Name: ${this.name} && his/her roll Number: ${this.roll}`
+      );
+    }
   }
+  const taskin = new NameStudent(
+    "Taskin Ahmad Al Faruqe",
+    242910701126,
+    "BSE in CST"
+  );
+  taskin.findStudent();
   //block scoping
 }

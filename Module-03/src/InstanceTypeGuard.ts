ea5cd1c714b57077e431ad19c2d61161567faaec
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2024-11-24 20:40:30 +0800

    Add Another Type Guard by using instance.

---
+++ Module-03/src/InstanceTypeGuard.ts
@@ -0,0 +1,14 @@
+{
+  // Block Scoping
+  class Student {
+    name: string;
+    roll: number;
+    StudyClass: string;
+    constructor(name: string, roll: number, StudyClass: string) {
+      this.name = name;
+      this.roll = roll;
+      this.StudyClass = StudyClass;
+    }
+  }
+  // Block Scoping
+}

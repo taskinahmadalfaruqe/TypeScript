e54e83c2dc5519afe1dff353bda04c77ce3ef8cc
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2024-11-25 23:39:05 +0800

    Add Instance

--- Module-03/src/InstanceTypeGuard.ts
+++ Module-03/src/InstanceTypeGuard.ts
@@ -10,5 +10,11 @@
       this.StudyClass = StudyClass;
     }
   }
+  const Student01 = new Student(
+    "Taskin Ahmad Al Faruqe",
+    242910701129,
+    "BSC in CST"
+  );
+  console.log(Student.name);
   // Block Scoping
 }

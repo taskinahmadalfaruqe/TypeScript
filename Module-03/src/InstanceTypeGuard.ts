e6b063199524b9c3a05a56614759fef09dfa6b96
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2024-11-26 21:03:47 +0800

    Add Instance Type Guard.

--- Module-03/src/InstanceTypeGuard.ts
+++ Module-03/src/InstanceTypeGuard.ts
@@ -9,12 +9,50 @@
       this.roll = roll;
       this.StudyClass = StudyClass;
     }
+    getRollNumber() {
+      console.log("I am a Student");
+    }
   }
-  const Student01 = new Student(
+  class SectionA extends Student {
+    constructor(name: string, roll: number, StudyClass: string) {
+      super(name, roll, StudyClass);
+    }
+    getSectionA() {
+      console.log("My Section Is: A");
+    }
+  }
+  class SectionB extends Student {
+    constructor(name: string, roll: number, StudyClass: string) {
+      super(name, roll, StudyClass);
+    }
+    getSectionB() {
+      console.log("My Section Is: B");
+    }
+  }
+  const isSectionA = (student: Student): student is SectionA => {
+    return student instanceof SectionA;
+  };
+  const isSectionB = (student: Student): student is SectionB => {
+    return student instanceof SectionB;
+  };
+  const GetStudent = (student: Student) => {
+    if (isSectionA(student)) {
+      student.getSectionA();
+    } else if (isSectionB(student)) {
+      student.getSectionB();
+    }
+  };
+  const Student01 = new SectionA(
     "Taskin Ahmad Al Faruqe",
-    242910701129,
+    242910701126,
     "BSC in CST"
   );
-  console.log(Student.name);
+  const Student02 = new SectionB(
+    "Taskin Ahmad Al Faruqe",
+    242910701126,
+    "BSC in CST"
+  );
+  GetStudent(Student01);
+  // console.log(Student.name);
   // Block Scoping
 }

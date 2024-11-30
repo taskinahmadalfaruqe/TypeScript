953ea7218ae2863d0abdcf382fbeb2d8c6de44b3
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-11-30 18:30:44 +0600

    Update opp

---
+++ Module-03/src/opp.ts
@@ -0,0 +1,22 @@
+  class Person {
+    getSleep() {
+      console.log("i am sleeping 8 hours");
+    }
+  }
+  class Student extends Person {
+    getSleep() {
+      console.log("I am sleeping 7 hours");
+    }
+  }
+  class Developer extends Person {
+    getSleep() {
+      console.log("I am sleeping 6 hours");
+    }
+  }
+  const getSleepTime = (perms: Person) => {
+    return perms.getSleep();
+  };
+  const Person01 = new Person();
+  const Person02 = new Student();
+  const Person03 = new Developer();
+  getSleepTime(Person02);

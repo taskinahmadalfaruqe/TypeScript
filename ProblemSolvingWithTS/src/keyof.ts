dff71dea71b345efa966756fbbb8b91bf9f3d465
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2024-12-05 21:34:17 +0800

    Add Type OF.

--- ProblemSolvingWithTS/src/keyof.ts
+++ ProblemSolvingWithTS/src/keyof.ts
@@ -1,28 +1,29 @@
 {
   // Block Scoping
+  
   // Define a simple Book type
-  type Book = {
+  type BookCategory = {
     title: string;
     author: string;
     yearPublished: number;
   };
 
   // Use `keyof` to get the keys of the Book type
-  type BookKeys = keyof Book; // "title" | "author" | "yearPublished"
+  type BookKeys = keyof BookCategory; 
 
   // Function that takes a Book and a key, and returns the value of that key
   function getBookProperty<T, K extends keyof T>(book: T, key: K): T[K] {
     return book[key];
   }
 
-  const myBook: Book = {
-    title: "The Great Typescript",
-    author: "Mezbaul Abedin Persian",
-    yearPublished: 2025,
+  const myBook: BookCategory = {
+    title: "The Programming World",
+    author: "Programming Hero",
+    yearPublished: 2024,
   };
 
   // We can only pass valid keys of `Book`
-  console.log(getBookProperty(myBook, "title")); // "The Great Gatsby"
-  // console.log(getBookProperty(myBook, "pages")); // Error: Argument of type '"pages"' is not assignable to parameter of type '"title" | "author" | "yearPublished"'
+  console.log(getBookProperty(myBook, "title")); 
+
   // Block Scoping
 }

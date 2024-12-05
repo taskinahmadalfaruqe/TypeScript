93a78f70b65f54abd62d9405a0c7592c70a2e49f
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2024-12-05 21:40:27 +0800

    Add Ist Problem.

---
+++ Assignment01/src/welcome.ts
@@ -0,0 +1,9 @@
+{
+  //Block Scoping
+  const GetWelcome = (): string => {
+    return "Hello World, I will complete this course successfully and become a Next level Web Developer!";
+  };
+  const welcomeMessage = GetWelcome();
+  console.log(welcomeMessage);
+  //Block Scoping
+}

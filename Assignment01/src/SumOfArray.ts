6fa1363ff1758b535f738217c214905b86b8803a
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2024-12-06 19:48:49 +0800

    Add Summation of array.

--- Assignment01/src/SumOfArray.ts
+++ Assignment01/src/SumOfArray.ts
@@ -1,5 +1,13 @@
 {
   // Block Scoping.
-  
+  const sumArray = (array: number[]) => {
+    let summationValue: number = 0;
+    array.map((value) => {
+      summationValue += value;
+    });
+    return summationValue;
+  };
+  const summation = sumArray([1, 2, 3]);
+  console.log(summation);
   // Block Scoping.
 }

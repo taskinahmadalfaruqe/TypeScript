533e7b88b336800b01a5cd3618f1ced449853b3c
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-06-27 22:55:50 +0800

    Add Data Type In Ts.

---
+++ Module-01/src/1.4_DataType.ts
@@ -0,0 +1,28 @@
+//string
+let fastname: string = 'Taskin';
+console.log(fastname);
+
+// Numbner
+let numberData: number = 74;
+console.log(numberData);
+
+
+//boolean
+let booleanData: boolean = true;
+console.log(booleanData);
+
+// null
+let NullData: null = null;
+console.log(NullData);
+
+// Undeffien
+let UndeffienData: undefined = undefined;
+console.log(UndeffienData);
+
+
+// Array
+let FriendsArray: string[] = ["Taskin", "Rana", "Humaira", "Bosir", "Sabbir"]
+
+
+//Tuple
+let TupleArray: [string, number] = ["Taskin", 52];

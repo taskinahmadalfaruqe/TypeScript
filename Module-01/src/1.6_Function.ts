e44e7908aac9f3dc64d6a2db6dcca241fc38d2dc
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-06-30 22:12:17 +0800

    add Function.

---
+++ Module-01/src/1.6_Function.ts
@@ -0,0 +1,10 @@
+// Learning Function
+// 01 normal function
+// 02 Arrow Function 
+function addNumber(num1: number, num2: number) {
+    num1 + num2;
+}
+addNumber(1, 2)
+
+const AddNumberA = (num01: number, num02: number) => num01 + num02;
+console.log(AddNumberA(2, 3));

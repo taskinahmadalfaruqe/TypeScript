d44f4eae99f046ea6c6c87172c994e96c9645917
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2025-08-08 23:53:14 +0800

    Add Constraints.ts.

---
+++ Module-02/src/2.6_Constraints.ts
@@ -0,0 +1,19 @@
+{
+  // Block Spacing
+    const getArray = <T>(data: T): [T] => {
+    return [data];
+  };
+
+  const res01 = getArray<string>("Taskin");
+  const res02 = getArray<number>(242910701126);
+  const res03 = getArray<boolean>(true);
+
+  // Generic With Tuple Function
+  const getTuple = <T, X>(Data01: T, Data02: X): [T, X] => {
+    return [Data01, Data02];
+  };
+
+  const repTuple01 = getTuple<string, number>("Taskin", 212566);
+  
+  // Block Spacing
+}

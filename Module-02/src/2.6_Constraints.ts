35a11c2b7af1c474ff63f4d3dedd0930008be5f2
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2025-08-09 23:46:28 +0800

    Add Constraints .ts

--- Module-02/src/2.6_Constraints.ts
+++ Module-02/src/2.6_Constraints.ts
@@ -1,19 +1,30 @@
 {
   // Block Spacing
-    const getArray = <T>(data: T): [T] => {
-    return [data];
+  //  Constrance
+
+  interface Student {
+    name: string;
+    id: number;
+    email: string;
+  }
+  const AddCourse = <T extends Student>(student: T) => {
+    const AddNedCorse = "Next Level Developer";
+    return {
+      ...student,
+      AddNedCorse,
+    };
   };
 
-  const res01 = getArray<string>("Taskin");
-  const res02 = getArray<number>(242910701126);
-  const res03 = getArray<boolean>(true);
-
-  // Generic With Tuple Function
-  const getTuple = <T, X>(Data01: T, Data02: X): [T, X] => {
-    return [Data01, Data02];
-  };
-
-  const repTuple01 = getTuple<string, number>("Taskin", 212566);
-  
+  const student01 = AddCourse({
+    name: "Taskin",
+    id: 1,
+    email: "taskin@example.com",
+  });
+  const student02 = AddCourse({
+    name: "John",
+    id: 2,
+    email: "john@example.com",
+    enrollment: "enrolled",
+  });
   // Block Spacing
 }

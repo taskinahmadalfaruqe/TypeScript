70ed3ec35188d1936fc0cf6ab11648f5703956e9
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2025-08-15 23:33:57 +0800

    Add New code.

--- Module-02/src/2.7_GenericConsEWithKeyOFOperator.ts
+++ Module-02/src/2.7_GenericConsEWithKeyOFOperator.ts
@@ -1,10 +1,25 @@
 {
-    // Block Socping
+  // Block Socping
 
-    type Veichal={
-        car:string;
-        bike:string;
-        truck:string;
-    }
-    // Block Socping
+  type Veichal = {
+    car: string;
+    bike: string;
+    truck: string;
+  };
+
+  type mobile = {
+    Oneplus: string;
+    Apple: string;
+    Samsung: string;
+  };
+  type data01 = "Apple" | "Samsung" | "Oneplus";
+  type data02 = keyof mobile;
+  const mobleUser: data02 = "Apple";
+  const User01: data01 = "Samsung";
+
+  const AllDataFunction = (data: string): string => {
+    return data;
+  };
+  AllDataFunction(User01);
+  // Block Socping
 }

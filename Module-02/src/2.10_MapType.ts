0657ab8d536036137e0fc3b35a81755dd02a70e8
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2025-08-29 22:13:46 +0800

    Add Map Type.

---
+++ Module-02/src/2.10_MapType.ts
@@ -0,0 +1,9 @@
+{
+    // Block Scoping
+
+    // Map Type
+    const numberOfArray: number[] = [1, 2, 3, 4, 5];
+    const stringArray: string[] = numberOfArray.map(String);
+    console.log(stringArray)
+    // Block Scoping
+}
